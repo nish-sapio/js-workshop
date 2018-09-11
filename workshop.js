2dd5d10249f3e339c00f497f32530f4a547979e2
Nishant <nishant23799@gmail.com>
2018-09-12 03:00:27 +0530

    operators

--- workshop.js
+++ workshop.js
@@ -1,34 +1,37 @@
-// DATA TYPES
+// OPERATORS
 
-var test;
+//LOGICAL OPERATORS
 
-console.log(typeof(test));
+var c1 , c2 , c3, c4, c5, c6, c7, c8;
 
-test = 12.25;
-
-console.log(typeof(test));
-
-test = 102;
-
-console.log(typeof(test));
-
-test = true;
-
-console.log(typeof(test));
-
-test = "Javascript";
-
-console.log(typeof(test));
-
-test = {
-	key1: 12,
-	key2 : "test"
-};
-
-console.log(typeof(test));
+//PLAYING WITH OR OPERATOR
+c1 = 3 || 4; //true || true 
+console.log("c1: " + c1);
+c2 = 5||0; //true || false
+console.log("c2: " + c2);
+c3 = 0||3; //false || true
+console.log("c3: " + c3);
+c4 = 0||0; //false || false
+console.log("c4: " + c4);
 
 
-test = null;
+//PLAYING WITH AND GATE
+c5 = 3 && 4; //true && true 
+console.log("c5: " + c5);
+c6 = 5&&0; //true && false
+console.log("c6: " + c6);
+c7 = 0&&3; //false && true
+console.log("c7: " + c7);
+c8 = 0&&0; //false && false
+console.log("c8: " + c8);
 
-console.log(typeof(test));
 
+//THE COMPARISON OPERATOR
+var a = "2";
+var b = 2;
+
+if(a == b)
+	console.log("hello");
+
+if(a===b)
+	console.log("bye");
