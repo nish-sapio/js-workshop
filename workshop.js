2697d119a4d900b67df878c1eefa60eb8f93de30
Nishant <nishant23799@gmail.com>
2018-09-12 03:08:51 +0530

    coercion

--- workshop.js
+++ workshop.js
@@ -1,37 +1,12 @@
-// OPERATORS
+//COERCION
 
-//LOGICAL OPERATORS
+// "+" IS THE CONCATENATION OPERATOR AND GIVES THE PRIORITY TO THE STRING DATA TYPE
+var string = "42";
+var number = 2;
 
-var c1 , c2 , c3, c4, c5, c6, c7, c8;
+console.log(string + number);
 
-//PLAYING WITH OR OPERATOR
-c1 = 3 || 4; //true || true 
-console.log("c1: " + c1);
-c2 = 5||0; //true || false
-console.log("c2: " + c2);
-c3 = 0||3; //false || true
-console.log("c3: " + c3);
-c4 = 0||0; //false || false
-console.log("c4: " + c4);
+// "*" IS THE MULTIPLICATION OPERATOR AND GIVES THE PRIORITY TO THE NUMBER DATA TYPE
 
-
-//PLAYING WITH AND GATE
-c5 = 3 && 4; //true && true 
-console.log("c5: " + c5);
-c6 = 5&&0; //true && false
-console.log("c6: " + c6);
-c7 = 0&&3; //false && true
-console.log("c7: " + c7);
-c8 = 0&&0; //false && false
-console.log("c8: " + c8);
-
-
-//THE COMPARISON OPERATOR
-var a = "2";
-var b = 2;
-
-if(a == b)
-	console.log("hello");
-
-if(a===b)
-	console.log("bye");
+console.log(string * number);
+console.log("in case you are wondering the data type of the output: its a " + typeof(string*number));
