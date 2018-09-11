2223b192673d78778e7c6a9b471e98a5812d7c99
Kailash Bhalaki <kailashbhalaki@gmail.com>
2018-09-12 03:39:04 +0530

    if-2-ternary

--- workshop.js
+++ workshop.js
@@ -1,48 +1,7 @@
-// Conditionals
+//Conditionals
+// Ternary - Statements
+var b = true;
+var a = ( b  ? "4" : "5" );
 
-// if
-
-var a = 2; // Number
-var b = "2"; // String
-
-
-if (a == 2) {
-    console.log("equal1");
-} else if (a == 3) {
-    console.log("a is not 3");
-} else {
-    console.log("not-equal1")
-}
-
-
-
-if (a === 2) {
-    console.log("equal2");
-} else {
-    console.log("not-equal2")
-}
-
-
-
-if (b == 2) {
-    console.log("equal3");
-} else {
-    console.log("not-equal3")
-}
-
-
-
-if (b === 2) {
-    console.log("equal4");
-} else {
-    console.log("not-equal4")
-}
-
-
-
-
-if (a == 2 || a === 2) {
-    console.log("equal5");
-} else {
-    console.log("not-equal5")
-}
+console.log("b:", b);
+console.log("a:", a);
