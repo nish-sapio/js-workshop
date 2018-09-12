0937977be5c29935aa1f3113d5ae926adc447cf0
Nishant <nishant23799@gmail.com>
2018-09-12 17:21:21 +0530

    scoping

--- workshop.js
+++ workshop.js
@@ -1,44 +1,28 @@
-// functions
+//Scoping
 
+var a = 25;
 
-function greetName(name) {
-    console.log("My name is ", name);
+(function stage1(a){
+	console.log("The value of a in stage 1 : " + a); 
+})(10);
+
+(function stage2(){
+	var a = 20;
+	console.log("The value of a in stage 2: " + a);
+})();
+
+console.log("The value of a declared globally is:" + a);
+
+if(1){
+	let a = 50;
+	console.log("The value of a in if block with let: " + a);
 }
-greetName('<yourName>');
 
-// assigning functions to variables. (No practical difference)
+console.log("The value of a declared globally is:" + a);
 
-var greetFunction = function (name) {
-    console.log("My name is ", name);
+if(1){
+	a = 40;
+	console.log("The value of a in if block without let: " + a);
 }
-greetFunction('Kailash');
 
-// names is an array
-var names = ['name0', 'name1', 'name2', 'name3']
-
-// forEach is a loop in Js that takes a function as a parameter.
-
-function greetNames(names) {
-    // greetNames takes array as a parameter.
-    names.forEach(function (name) { // here this function is an anonymous function i.e with no name.
-        console.log(name); //  function is called for each element in the array. 
-    });
-}
-greetNames(names);
-
-function greetNames2 (names) {
-    names.forEach(name => { // arrow notation of a function name is a parameter, as above. (Not any difference)
-        console.log(name);
-    });
-}
-greetNames2(names);
-
-// Immediately Invoked Function Expressions (IIFE)
-
-// These functions need not be executed seperately, they are called as soon as function is completed.
-
-(function (name) {
-    console.log('My name is ', name);
-})('Javascript');
-
-
+console.log( "The value of a declared globally is after if statements:" + a);
