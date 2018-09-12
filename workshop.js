d2775cc369f0f550b061eb2957bc1dff69935f7e
Kailash Bhalaki <kailashbhalaki@gmail.com>
2018-09-12 16:45:37 +0530

    functions and forEach

--- workshop.js
+++ workshop.js
@@ -1,16 +1,44 @@
-// loops
-// for 
-// while 
+// functions
 
-// similar to loops in C .
-for (i = 0; i<5; i++) {
-    console.log('The value is', i);
-}
 
-var i = 0; 
-while ( i < 5) {
-    console.log('The value of i is', i);
-    i++;
+function greetName(name) {
+    console.log("My name is ", name);
 }
+greetName('<yourName>');
+
+// assigning functions to variables. (No practical difference)
+
+var greetFunction = function (name) {
+    console.log("My name is ", name);
+}
+greetFunction('Kailash');
+
+// names is an array
+var names = ['name0', 'name1', 'name2', 'name3']
+
+// forEach is a loop in Js that takes a function as a parameter.
+
+function greetNames(names) {
+    // greetNames takes array as a parameter.
+    names.forEach(function (name) { // here this function is an anonymous function i.e with no name.
+        console.log(name); //  function is called for each element in the array. 
+    });
+}
+greetNames(names);
+
+function greetNames2 (names) {
+    names.forEach(name => { // arrow notation of a function name is a parameter, as above. (Not any difference)
+        console.log(name);
+    });
+}
+greetNames2(names);
+
+// Immediately Invoked Function Expressions (IIFE)
+
+// These functions need not be executed seperately, they are called as soon as function is completed.
+
+(function (name) {
+    console.log('My name is ', name);
+})('Javascript');
 
 
